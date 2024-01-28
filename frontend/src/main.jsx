5e3eb155ead3a51479a3b85189af8bc453d4369f
Navneet <n4navneetanand@gmail.com>
2024-01-29 00:24:25 +0530

    fix scrolling issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { ShopContextProvider } from "./context/ShopContext.jsx";
+import { Layout } from "./components/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ShopContextProvider>
       <Router>
-        <App />
+        <Layout>
+          <App />
+        </Layout>
       </Router>
     </ShopContextProvider>
   </React.StrictMode>
